ddcfb4f24f61a0edaf40bfa796f1ca59573986d3
Yana <bloom.hog@gmail.com>
2020-05-02 00:09:40 +0300

    feat: add swiper.js

---
+++ movie-search/src/swiper.js
@@ -0,0 +1,26 @@
+﻿const mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  slidesPerColumnFill: 'row',
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+export default function createSlide() {
+  for (let i = 0; i < 10; i += 1) {
+    mySwiper.appendSlide('<div class="swiper-slide"></div>')
+  }
+}
