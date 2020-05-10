aff5f2c8e0c87094c853f2e97b6fbd1308401e25
Yana <bloom.hog@gmail.com>
2020-05-10 19:06:25 +0300

    feat: add test for itemSlideCount

---
+++ movie-search/test/itemSlide.test.js
@@ -0,0 +1,27 @@
+﻿import itemSlideCount from './itemSlide'
+
+describe('item slide count', () => {
+  test('result - 3 item on last page', () => {
+    const page = 3
+    const resultCount = 23
+    expect(itemSlideCount(page, resultCount)).toBe(3)
+  })
+
+  test('result - 8 item on last page', () => {
+    const page = 19
+    const resultCount = 188
+    expect(itemSlideCount(page, resultCount)).toBe(8)
+  })
+
+  test('result - 10 item on last page', () => {
+    const page = 9
+    const resultCount = 90
+    expect(itemSlideCount(page, resultCount)).toBe(10)
+  })
+
+  test('result - 10 item on last page', () => {
+    const page = 1
+    const resultCount = 10
+    expect(itemSlideCount(page, resultCount)).toBe(10)
+  })
+})
