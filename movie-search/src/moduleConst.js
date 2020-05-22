fe51ced4eaeaad097ea98bbb0948164a12408964
Yana <bloom.hog@gmail.com>
2020-05-22 18:04:31 +0300

    feat: add consts

--- movie-search/src/moduleConst.js
+++ movie-search/src/moduleConst.js
@@ -10,7 +10,12 @@ const VOICE_BTN = document.getElementById('btn_voice');
 const keyboard = document.createElement('div');
 const keyboardKeys = document.createElement('div');
 const delayDots = 300;
-const systemKeys = [13, 14, 27, 28, 41, 42, 54, 55, 56, 57, 58, 59, 63]
+const systemKeys = [13, 14, 27, 28, 41, 42, 54, 55, 56, 57, 58, 59, 63];
+const translateUrl = `https://translate.yandex.net/api/v1.5/tr.json/translate?`;
+const translateKey = `key=trnsl.1.1.20200502T220917Z.cd9bb434abbf37f3.c07bdee9e96c935d70f599242ba2dd8c64fd0f9b&text=`;
+const urlAPI = `https://www.omdbapi.com/?s=`;
+const urlAPIkey = `&apikey=24f0fb79`;
+const urlAPIrait = `https://www.omdbapi.com/?i=`
 
 const isCyrillic = function checkLang(text) {
   return /[а-я]/i.test(text);
@@ -19,4 +24,5 @@ const isCyrillic = function checkLang(text) {
 export {
   KEYBOARD_CONTAINER, INPUT, LOADER_CONTAINER, SEARCH_BTN, keyboard, keyboardKeys, systemKeys,
   CLEAR_BTN, KEYBOARD_BTN, RESULT_CONTAINER, DOTS_CONTAINER, VOICE_BTN, isCyrillic, delayDots,
+  translateUrl, translateKey, urlAPI, urlAPIkey, urlAPIrait,
 }
