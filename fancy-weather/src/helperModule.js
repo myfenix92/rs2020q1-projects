98e2c22f723667859283459e0ff37e0499af8260
Yana <bloom.hog@gmail.com>
2020-05-31 20:51:16 +0300

    feat: add helper module

---
+++ fancy-weather/src/helperModule.js
@@ -0,0 +1,22 @@
+﻿import { REFRESH } from './constModule';
+
+function viewHelp() {
+  document.querySelector('.help').style.display = 'block';
+}
+
+document.getElementById('helper').addEventListener('mouseover', viewHelp);
+document.getElementById('helper').addEventListener('mouseout', () => {
+  document.querySelector('.help').style.display = 'none';
+});
+
+function GetRandom() {
+  const max = 1000000;
+  const min = 1;
+  return Math.floor(Math.random() * max + min);
+}
+
+function activeClassResresh() {
+  REFRESH.classList.remove('active_refresh');
+}
+
+export { GetRandom, activeClassResresh };
